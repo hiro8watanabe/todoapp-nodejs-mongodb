b91566f25b394d65fad5798038b18486ec81f633
hiro <onecast.onelost615@gmail.com>
2023-01-15 01:07:11 +0900

    MongoDBに接続

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const taskRoute = require("./routers/tasks");
+const connectDB = require("./db/connect");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+mongoose.set("strictQuery", false);
 
 const PORT = 3000;
 
 //ルーティングの処理
 app.use("/api/v1/tasks", taskRoute);
 
-app.listen(PORT, console.log("Nodeサーバー起動"));
+//データベースと接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log("Nodeサーバー起動"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
